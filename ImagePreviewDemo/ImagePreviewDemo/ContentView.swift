aff065c08ce8cf33a625bdbde5f16b406e9e84c4
GorCat <>
2023-04-19 14:21:53 +0800

    optimized single image preview

--- ImagePreviewDemo/ImagePreviewDemo/ContentView.swift
+++ ImagePreviewDemo/ImagePreviewDemo/ContentView.swift
@@ -11,16 +11,36 @@ struct ContentView: View {
     @State var showImageViewer: Bool = false
     @State var imgURL: String = "https://ilivegirl.s3.amazonaws.com/photos/7bf24171b485a35b4005304d25a316cb.jpg"
     
+    @State var images = [
+            "https://r.iwlive.club/photos/cfd29a04e8817f92698816085488921b.jpg",
+            "https://r.iwlive.club/photos/0e4fe405f66d34a23e01b1ecbdf06535.jpg",
+            "https://r.iwlive.club/photos/b2ccfb7e1b7863888278c3299757f5ba.jpg",
+            "https://r.iwlive.club/photos/b5fd62f48248486310cf9e1be4df2d2f.jpg",
+            "https://r.iwlive.club/22m/AndroidImage/1679378222628_my_pic1679378222396.jpg",
+            "https://r.iwlive.club/22m/AndroidImage/1679378232431_my_pic1679378231890.jpg",
+            "https://r.iwlive.club/22m/AndroidImage/1679378244214_my_pic1679378244129.jpg",
+            "https://r.iwlive.club/22m/AndroidImage/1679378251982_my_pic1679378251663.jpg"]
+    
     var body: some View {
-        VStack {
+        ZStack {
             Text("Example!")
                 .onTapGesture {
                     showImageViewer = true
                 }
+            
+            if showImageViewer {
+                ImageViewerRemote(URL(string: images[1])!) {
+                    Rectangle()
+                        .fill(Color.white)
+                        .overlay(
+                            Image("image_placeholder")
+                        )
+                }
+            }
         }
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .overlay(ImageViewerRemote(imageURL: self.$imgURL, viewerShown: self.$showImageViewer))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- ImagePreviewDemo/ImagePreviewDemo/main/ImageViewerRemote.swift
+++ ImagePreviewDemo/ImagePreviewDemo/main/ImageViewerRemote.swift
@@ -4,162 +4,61 @@ import SDWebImageSwiftUI
 import Combine
 
 @available(iOS 13.0, *)
-public struct ImageViewerRemote: View {
-    @Binding var viewerShown: Bool
-    @Binding var imageURL: String
-    @State var httpHeaders: [String: String]?
-    @State var disableCache: Bool?
-    @State var caption: Text?
-    @State var closeButtonTopRight: Bool?
+public struct ImageViewerRemote<Placeholder: View>: View {
+    private var imageURL: URL
+    private var placeholderView: Placeholder
     
-    var aspectRatio: Binding<CGFloat>?
+    @State private var dragOffset: CGSize = CGSize.zero
+    @State private var dragOffsetPredicted: CGSize = CGSize.zero
+    @State private var progress: Int = 0
     
-    @State var dragOffset: CGSize = CGSize.zero
-    @State var dragOffsetPredicted: CGSize = CGSize.zero
-    
-    @ObservedObject var loader: ImageLoader
-    
-    public init(imageURL: Binding<String>, viewerShown: Binding<Bool>, aspectRatio: Binding<CGFloat>? = nil, disableCache: Bool? = nil, caption: Text? = nil, closeButtonTopRight: Bool? = false) {
-        _imageURL = imageURL
-        _viewerShown = viewerShown
-        _disableCache = State(initialValue: disableCache)
-        self.aspectRatio = aspectRatio
-        _caption = State(initialValue: caption)
-        _closeButtonTopRight = State(initialValue: closeButtonTopRight)
-        
-        loader = ImageLoader(url: imageURL)
+    public init(_ url: URL, @ViewBuilder placeholderView: () -> Placeholder) {
+        self.imageURL = url
+        self.placeholderView = placeholderView()
     }
 
     @ViewBuilder
     public var body: some View {
-        VStack {
-            if(viewerShown && imageURL.count > 0) {
-                ZStack {
-                    VStack {
-                        HStack {
-                              
-                            if self.closeButtonTopRight == true {
-                                Spacer()
-                            }
-                            
-                            Button(action: { self.viewerShown = false }) {
-                                Image(systemName: "xmark")
-                                    .foregroundColor(Color(UIColor.white))
-                                    .font(.system(size: UIFontMetrics.default.scaledValue(for: 24)))
-                            }
-                            
-                            
-                            if self.closeButtonTopRight != true {
-                                Spacer()
-                            }
-                        }
-                        
-                        Spacer()
-                    }
-                    .padding()
-                    .zIndex(2)
+        ZStack {
+            WebImage(url: imageURL)
+                .onProgress { (installed, total) in
                     
-                    VStack {
-                        ZStack {
-                            if(self.disableCache == nil || self.disableCache == false) {
-                                
-                                WebImage(url: URL(string: "https://via.placeholder.com/150.png")!)
-                                //                                                    .placeholder {
-                                //                                                        placeholderView
-                                //                                                    }
-                                    .resizable()
-                                    .aspectRatio(self.aspectRatio?.wrappedValue, contentMode: .fit)
-                                    .offset(x: self.dragOffset.width, y: self.dragOffset.height)
-                                    .rotationEffect(.init(degrees: Double(self.dragOffset.width / 30)))
-                                    .pinchToZoom()
-                                    .gesture(DragGesture()
-                                        .onChanged { value in
-                                            self.dragOffset = value.translation
-                                            self.dragOffsetPredicted = value.predictedEndTranslation
-                                        }
-                                        .onEnded { value in
-                                            if((abs(self.dragOffset.height) + abs(self.dragOffset.width) > 570) || ((abs(self.dragOffsetPredicted.height)) / (abs(self.dragOffset.height)) > 3) || ((abs(self.dragOffsetPredicted.width)) / (abs(self.dragOffset.width))) > 3) {
-                                                withAnimation(.spring()) {
-                                                    self.dragOffset = self.dragOffsetPredicted
-                                                }
-                                                self.viewerShown = false
-                                                return
-                                            }
-                                            withAnimation(.interactiveSpring()) {
-                                                self.dragOffset = .zero
-                                            }
-                                        }
-                                    )
-                                //                                })
-                            }
-                            else {
-                                if loader.image != nil {
-                                    Image(uiImage: loader.image!)
-                                        .resizable()
-                                        .aspectRatio(self.aspectRatio?.wrappedValue, contentMode: .fit)
-                                        .offset(x: self.dragOffset.width, y: self.dragOffset.height)
-                                        .rotationEffect(.init(degrees: Double(self.dragOffset.width / 30)))
-                                        .pinchToZoom()
-                                        .gesture(DragGesture()
-                                            .onChanged { value in
-                                                self.dragOffset = value.translation
-                                                self.dragOffsetPredicted = value.predictedEndTranslation
-                                            }
-                                            .onEnded { value in
-                                                if((abs(self.dragOffset.height) + abs(self.dragOffset.width) > 570) || ((abs(self.dragOffsetPredicted.height)) / (abs(self.dragOffset.height)) > 3) || ((abs(self.dragOffsetPredicted.width)) / (abs(self.dragOffset.width))) > 3) {
-                                                    withAnimation(.spring()) {
-                                                        self.dragOffset = self.dragOffsetPredicted
-                                                    }
-                                                    self.viewerShown = false
-                                                    return
-                                                }
-                                                withAnimation(.interactiveSpring()) {
-                                                    self.dragOffset = .zero
-                                                }
-                                            }
-                                        )
-                                }
-                                else {
-                                    Text(":/")
-                                }
-                            }
-                            
-                            if(self.caption != nil) {
-                                VStack {
-                                    Spacer()
-                                    
-                                    VStack {
-                                        Spacer()
-                                        
-                                        HStack {
-                                            Spacer()
-                                            
-                                            self.caption
-                                                .foregroundColor(.white)
-                                                .multilineTextAlignment(.center)
-                                            
-                                            Spacer()
-                                        }
-                                    }
-                                    .padding()
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                }
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            }
+                    guard installed > 0, total > 0 else {
+                        return
+                    }
+                    progress = installed * 100 / total // 0~100
+                }
+                .placeholder {
+                    placeholderView
+                }
+                .resizable()
+                .offset(x: self.dragOffset.width, y: self.dragOffset.height)
+                .rotationEffect(.init(degrees: Double(self.dragOffset.width / 30)))
+                .pinchToZoom()
+                .gesture(DragGesture()
+                    .onChanged { value in
+                        self.dragOffset = value.translation
+                        self.dragOffsetPredicted = value.predictedEndTranslation
+                    }
+                    .onEnded { value in
+                        withAnimation(.interactiveSpring()) {
+                            self.dragOffset = .zero
                         }
                     }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color(red: 0.12, green: 0.12, blue: 0.12, opacity: (1.0 - Double(abs(self.dragOffset.width) + abs(self.dragOffset.height)) / 1000)).edgesIgnoringSafeArea(.all))
-                    .zIndex(1)
-                }
-                .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.2)))
-                .onAppear() {
-                    self.dragOffset = .zero
-                    self.dragOffsetPredicted = .zero
-                }
+                )
+            
+            if progress != 100 {
+                
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 0.12, green: 0.12, blue: 0.12, opacity: (1.0 - Double(abs(self.dragOffset.width) + abs(self.dragOffset.height)) / 1000)).edgesIgnoringSafeArea(.all))
+        .zIndex(1)
+        .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.2)))
+        .onAppear() {
+            self.dragOffset = .zero
+            self.dragOffsetPredicted = .zero
+        }
     }
 }
 
@@ -313,48 +212,5 @@ struct PinchToZoom: ViewModifier {
 extension View {
     func pinchToZoom() -> some View {
         self.modifier(PinchToZoom())
-    }
-}
-
-
-
-
-
-
-class ImageLoader: ObservableObject {
-    @Published var image: UIImage?
-    private let url: Binding<String>
-    private var cancellable: AnyCancellable?
-    
-    func getURLRequest(url: String) -> URLRequest {
-        let url = URL(string: url) ?? URL(string: "https://via.placeholder.com/150.png")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        return request;
-    }
-
-    init(url: Binding<String>) {
-        self.url = url
-        
-        if(url.wrappedValue.count > 0) {
-            load()
-        }
-    }
-    
-    deinit {
-        cancellable?.cancel()
-    }
-
-    func load() {
-        cancellable = URLSession.shared.dataTaskPublisher(for: getURLRequest(url: self.url.wrappedValue))
-            .map { UIImage(data: $0.data) }
-            .replaceError(with: nil)
-            .receive(on: DispatchQueue.main)
-            .assign(to: \.image, on: self)
-    }
-    
-    func cancel() {
-        cancellable?.cancel()
     }
 }
