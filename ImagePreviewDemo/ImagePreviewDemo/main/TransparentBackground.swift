b151d1d945b07151fae42c465885cfb8d61540e9
GorCat <>
2023-04-19 15:56:51 +0800

    add TransparentBackground

---
+++ ImagePreviewDemo/ImagePreviewDemo/main/TransparentBackground.swift
@@ -0,0 +1,21 @@
+//
+//  TransparentBackground.swift
+//  ImagePreviewDemo
+//
+//  Created by GorCat on 2023/4/19.
+//
+
+import SwiftUI
+
+struct TransparentBackground: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
