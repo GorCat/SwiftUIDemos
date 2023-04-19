eb0904e77b208c3f4371c295fd33ea190f1c5e0b
GorCat <>
2023-04-19 10:44:02 +0800

    add images preview demo

---
+++ ImagePreviewDemo/ImagePreviewDemo/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ImagePreviewDemo
+//
+//  Created by GorCat on 2023/4/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var showImageViewer: Bool = true
+    @State var imgURL: String = "https://ilivegirl.s3.amazonaws.com/photos/7bf24171b485a35b4005304d25a316cb.jpg"
+    
+    var body: some View {
+        VStack {
+            Text("Example!")
+        }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay(ImageViewerRemote(imageURL: self.$imgURL, viewerShown: self.$showImageViewer))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImagePreviewDemo/ImagePreviewDemo/ImagePreviewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePreviewDemoApp.swift
+//  ImagePreviewDemo
+//
+//  Created by GorCat on 2023/4/19.
+//
+
+import SwiftUI
+
+@main
+struct ImagePreviewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
