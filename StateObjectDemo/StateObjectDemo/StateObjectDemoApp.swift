e2322c2cb018531a9f48bf490b36b340a6d3327a
GorCat <>
2023-05-07 17:16:38 +0800

    add stateobject demo

---
+++ StateObjectDemo/StateObjectDemo/StateObjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectDemoApp.swift
+//  StateObjectDemo
+//
+//  Created by GorCat on 2023/5/7.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
