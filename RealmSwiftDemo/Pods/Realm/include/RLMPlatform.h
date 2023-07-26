989621c07999bbb94d5188a11fa08d7fba67787f
GorCat <>
2023-07-26 15:31:35 +0800

    add realm SDK

---
+++ RealmSwiftDemo/Pods/Realm/include/RLMPlatform.h
@@ -0,0 +1,3 @@
+#define REALM_IOPLATFORMUUID @"WARNING: The active Xcode command line tools, as returned by 'xcode-select -p', are not from Xcode.
+         The newest version of Xcode will be used instead.
+7C4ECDA9-AD0B-567F-94FD-BBBA7BD3ADDD"
