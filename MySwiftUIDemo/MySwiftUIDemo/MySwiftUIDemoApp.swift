a4c0adc5224a3e15f64aaab0e72fc89a5bcf57b8
GorCat <>
2023-07-26 15:28:47 +0800

    add Realm demo

---
+++ MySwiftUIDemo/MySwiftUIDemo/MySwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIDemoApp.swift
+//  MySwiftUIDemo
+//
+//  Created by GorCat on 2023/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RealmSwiftDemo/RealmSwiftDemo/RealmSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSwiftDemoApp.swift
+//  RealmSwiftDemo
+//
+//  Created by GorCat on 2023/7/26.
+//
+
+import SwiftUI
+
+@main
+struct RealmSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
